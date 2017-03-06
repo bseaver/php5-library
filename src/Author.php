f3d6c3c9622d7e8783a707a25ae819b283f0569f
Jake Campa and Jayeson Kiyabu <jayeson@>
2017-03-06 15:55:24 -0800

    add passing tests for save ugetSome and deleteSome for Author.php

--- src/Author.php
+++ src/Author.php
@@ -1,10 +1,12 @@
 <?php
   class Author{
     private $name;
+    private $id;
 
-    function __construct($name = '')
+    function __construct($name = '', $id = null)
     {
       $this->setName($name);
+      $this->setId($id);
     }
 
     function setName($new_name)
@@ -15,6 +17,79 @@
     function getName()
     {
       return $this->name;
+    }
+
+    function setId($new_id)
+    {
+      $this->id = (int) $new_id;
+    }
+
+    function getId()
+    {
+      return $this->id;
+    }
+
+    function save() {
+      $GLOBALS['DB']->exec("INSERT INTO authors (name) VALUES ('{$this->getName()}');");
+      $this->setId($GLOBALS['DB']->lastInsertId());
+    }
+
+    static function getSome($search_selector, $search_argument = '')
+    {
+        $output = array();
+        $statement_handle = null;
+        if ($search_selector == 'id') {
+            $statement_handle = $GLOBALS['DB']->prepare(
+                "SELECT * FROM authors WHERE id = :search_argument ORDER BY name, id;"
+            );
+            $statement_handle->bindValue(':search_argument', $search_argument, PDO::PARAM_STR);
+        }
+        if ($search_selector == 'name') {
+            $statement_handle = $GLOBALS['DB']->prepare(
+                "SELECT * FROM authors WHERE name = :search_argument ORDER BY name, id;"
+            );
+            $statement_handle->bindValue(':search_argument', $search_argument, PDO::PARAM_STR);
+        }
+        if ($search_selector == 'all') {
+            $statement_handle = $GLOBALS['DB']->prepare("SELECT * FROM authors ORDER BY id;");
+        }
+        if ($statement_handle) {
+          // var_dump($statement_handle);
+            $statement_handle->execute();
+            $results = $statement_handle->fetchAll();
+            // $results = $GLOBALS['DB']->query($query);
+            foreach ($results as $result) {
+                    $new_author = new Author(
+                    $result['name'],
+                    $result['id']
+                );
+                array_push($output, $new_author);
+            }
+        }
+        return $output;
+    }
+
+    static function deleteSome($search_selector, $search_argument = 0)
+    {
+        $statement_handle = null;
+        if ($search_selector == 'id') {
+            $statement_handle = $GLOBALS['DB']->prepare(
+                "DELETE FROM authors WHERE id = :search_argument;"
+            );
+            $statement_handle->bindValue(':search_argument', $search_argument, PDO::PARAM_INT);
+        }
+        if ($search_selector == 'name') {
+            $statement_handle = $GLOBALS['DB']->prepare(
+                "DELETE FROM authors WHERE name = :search_argument;"
+            );
+            $statement_handle->bindValue(':search_argument', $search_argument, PDO::PARAM_INT);
+        }
+        if ($search_selector == 'all') {
+            $statement_handle = $GLOBALS['DB']->prepare("DELETE FROM authors;");
+        }
+        if ($statement_handle) {
+            $statement_handle->execute();
+        }
     }
   }
 
--- tests/AuthorTest.php
+++ tests/AuthorTest.php
@@ -11,6 +11,11 @@
 
   class AuthorTest extends PHPUnit_Framework_TestCase
   {
+    protected function tearDown()
+    {
+      Author::deleteSome('all');
+    }
+
     function test_getter_setters_contructor()
     {
       //Arrange
@@ -23,6 +28,55 @@
 
       //Assert
       $this->assertEquals(['Bob'], [$author2->getName()]);
+    }
+
+    function test_save()
+    {
+      //Arrange
+      $name = "An Author";
+      $author1 = new Author($name, null);
+
+      //Act
+      $author1->save();
+      $result = Author::getSome('all', null);
+
+      //Assert
+      $this->assertEquals([$author1], $result);
+    }
+
+    function test_get_some_title()
+    {
+      //Arrange
+      $author1 = new Author("Author 1");
+      $author2 = new Author("Author 2");
+      $author3 = new Author("Author 3");
+      $author1->save();
+      $author2->save();
+      $author3->save();
+
+      //Act
+      $result = Author::getSome('name', "Author 1");
+
+      //Assert
+      $this->assertEquals([$author1], $result);
+    }
+
+    function test_delete_some_title()
+    {
+      //Arrange
+      $author1 = new Author("Author", null);
+      $author2 = new Author("Author", null);
+      $author3 = new Author("Author 3", null);
+      $author1->save();
+      $author2->save();
+      $author3->save();
+
+      //Act
+      Author::deleteSome('name', 'Author');
+      $result = Author::getSome('all');
+
+      //Assest
+      $this->assertEquals([$author3], $result);
     }
 
   }
